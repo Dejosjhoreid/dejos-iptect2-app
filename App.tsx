83ddd7a625d1544cfc303d0cd37850145d82fcbd
Dejosjhoreid <jadejos.student@asiancollege.edu.ph>
2026-04-19 10:01:06 +0800

    bodypage

--- App.tsx
+++ App.tsx
@@ -2,18 +2,23 @@ import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import Header from './src/components/header';
 import Footer from './src/components/footer';
+import BodyPage from './src/components/bodypage';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      
       {/* Header */}
       <Header name="Dejos" />
 
       {/* Content Area */}
-      <View style={styles.content} />
+      <View style={styles.content}>
+        <BodyPage />
+      </View>
 
       {/* Footer */}
       <Footer />
+
     </View>
   );
 }
